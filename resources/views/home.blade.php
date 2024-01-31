5d12e8b0199f8e5d5584d89691622fa28ddbecf0
Gangga Vidya Anggelika <ganggava@gmail.com>
2024-01-31 16:51:43 +0700

    finish

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,10 +7,37 @@
 <div class="container my-4">
     <div class="row">
         <div class="col-12">
-            <h2>Selamat Datang</h2>
+            <h2 class="text-center">Selamat Datang Disini</h2>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="/minuman.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title">DRINK</h5>
+                        <p class="card-text">Segar seperti sudah mandi.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="/cake.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title">CAKE</h5>
+                        <p class="card-text">Cake manis seperti janjimu.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="/makanan.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h5 class="card-title">FOODS</h5>
+                        <p class="card-text">Makan supaya hidup.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
-   
-   
+</div> 
 @endsection
